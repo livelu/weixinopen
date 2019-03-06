c4e78e43187c1bc80de264acc106a0720c4f593b
zhanglu@xinchanedu.com <zhanglu@xinchanedu.com>
2019-03-06 18:19:53 +0800

    00

--- tesy.py
+++ tesy.py
@@ -8,11 +8,11 @@ data = {
         'touser':'ox05N5V5GZaKqQ1Zy7-sP8DKX1Lo',
         'template_id':'fc-4PTEmhRCVj4WRSp-UTyBOvyvlr4snFMEtHS8h038',
         'page':page,
-        'form_id':'807abf2dfd3b4c3b8427766cde5b3b37',
+        'form_id':'e123e134f06b4601ad39a2103a78d812',
         'data':{
-            'keyword1':'张璐',
+            'keyword1':"张璐",
             'keyword2':time.strftime('%Y-%m-%d %H:%M:%S'),
-            'keyword3':'python发送',
+            'keyword3':"python发送",
         }
     }
 res = Wxkfpingtai.sendTemplate('19_EpajAbRpzQoIutm9QjLA9cdrQwxCdLt42xMBjEPvyJ_yZ0iGsILFAB0tsKLj0E1lPGo0Ur5UW106tfh0kvLpRxSSrYFzTzkTo_rgQHkjbfd-HaQMNBQ_ut2FO9Bib2PyAXjh8KFWuPLHrYT_OPMiAHDCDA',data)
