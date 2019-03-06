8a720a566bd8db9f294e909d027b952ecf3ae219
zhanglu@xinchanedu.com <zhanglu@xinchanedu.com>
2019-03-06 18:36:20 +0800

    00

--- tesy.py
+++ tesy.py
@@ -8,15 +8,14 @@ data = {
         'touser':'ox05N5V5GZaKqQ1Zy7-sP8DKX1Lo',
         'template_id':'fc-4PTEmhRCVj4WRSp-UTyBOvyvlr4snFMEtHS8h038',
         'page':page,
-        'form_id':'e123e134f06b4601ad39a2103a78d812',
+        'form_id':'b04c4be8b4574681800ab0e070db95b1',
         'data':{
-            'keyword1':"张璐",
-            'keyword2':time.strftime('%Y-%m-%d %H:%M:%S'),
-            'keyword3':"python发送",
+            'keyword1':{'value':'张璐'},
+            'keyword2':{'value':time.strftime('%Y-%m-%d %H:%M:%S')},
+            'keyword3':{'value':'python发送'},
         }
     }
 res = Wxkfpingtai.sendTemplate('19_EpajAbRpzQoIutm9QjLA9cdrQwxCdLt42xMBjEPvyJ_yZ0iGsILFAB0tsKLj0E1lPGo0Ur5UW106tfh0kvLpRxSSrYFzTzkTo_rgQHkjbfd-HaQMNBQ_ut2FO9Bib2PyAXjh8KFWuPLHrYT_OPMiAHDCDA',data)
-print(res)
-# print(res.status_code)
-# print(res.json())
-#wexin.()
+#print(res)
+print(res.status_code)
+print(res.json())
