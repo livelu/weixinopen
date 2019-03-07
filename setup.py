9caefb0a162206065e97efcccd7d779b85e29a02
zhanglu@xinchanedu.com <zhanglu@xinchanedu.com>
2019-03-07 10:57:32 +0800

    setup

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setup(
     packages = find_packages(),
     include_package_data = True,
     platforms = "weixin",
-    install_requires = ['pycurl']
+    install_requires = ['pycurl','requests']
 )
